af10d67d61005f6749c74749b28fab267d435d98
Ananya-999 <74000851+Ananya-999@users.noreply.github.com>
2025-10-17 23:35:16 +0530

    Create Constants.swift

---
+++ Constants.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+struct Constants {
+    static let apiKey = "DEMO_KEY_FOR_ASSIGNMENT"
+    static let baseURL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=\(apiKey)"
+}
